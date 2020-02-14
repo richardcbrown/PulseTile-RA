7fb1b4cbb7e8ae88925fdbc46daee531a418f4b5
Richard Brown <richardcharlesbrown88@gmail.com>
2020-02-14 12:00:09 +0000

    scatter chart

--- src/version/plugins/CarePlan/CarePlanResultsCharts.js
+++ src/version/plugins/CarePlan/CarePlanResultsCharts.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { VictoryCandlestick, VictoryAxis, VictoryChart } from 'victory';
+import { VictoryCandlestick, VictoryAxis, VictoryChart, VictoryScatter, VictoryLine } from 'victory';
 import moment from 'moment';
 
 export const CarePlanCandlestickChart = ({ carePlanChartData }) => {
@@ -7,6 +7,9 @@ export const CarePlanCandlestickChart = ({ carePlanChartData }) => {
     const { dataPoints } = carePlanChartData;
 
     const chartPoints = [];
+
+    const scatter1Points = [];
+    const scatter2Points = [];
 
     dataPoints.forEach((dp) => {
         chartPoints.push({
@@ -16,18 +19,42 @@ export const CarePlanCandlestickChart = ({ carePlanChartData }) => {
             open: dp.y0,
             high: dp.y0
         })
+
+        scatter1Points.push({ x: dp.x, y: dp.y0 })
+        scatter2Points.push({ x: dp.x, y: dp.y })
     });
 
     return (
         <VictoryChart
             domainPadding={{ x: [20, 20], y: [5, 5]}}
         >
-            <VictoryCandlestick 
+            {/* <VictoryCandlestick 
                 scale={{ x: "time" }}
                 data={chartPoints}
                 candleWidth={10}
                 style={{ data: { stroke: "#3596f4" } }}
                 candleColors={{ positive: "#3596f4", negative: "#3596f4" }}
+            /> */}
+            <VictoryScatter
+                scale={{ x: "time" }}
+                data={scatter1Points}
+                style={{ data: { fill: "#3596f4" } }}
+            />
+            <VictoryScatter 
+                scale={{ x: "time" }}
+                data={scatter2Points}
+                style={{ data: { fill: "#3596f4" } }}
+            />
+
+            <VictoryLine
+                scale={{ x: "time" }}
+                data={scatter1Points}
+                style={{ data: { stroke: "#3596f4" } }}
+            />
+            <VictoryLine 
+                scale={{ x: "time" }}
+                data={scatter2Points}
+                style={{ data: { stroke: "#3596f4" } }}
             />
             <VictoryAxis
                 fixLabelOverlap={true} 
