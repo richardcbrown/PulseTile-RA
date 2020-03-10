5998e3e746c4d5e4be2e16581ed86c31d335de49
Richard Brown <richardcharlesbrown88@gmail.com>
2020-03-10 08:52:10 +0000

    initial accordion view

--- src/version/pages/Terms/index.js
+++ src/version/pages/Terms/index.js
@@ -11,6 +11,11 @@ import backgroundImage from "../../images/Artboard.png";
 import Grid from "@material-ui/core/Grid";
 import Card from "@material-ui/core/Card";
 import GeneralDialog from "../../common/Dialogs/GeneralDialog";
+import ExpansionPanel from "@material-ui/core/ExpansionPanel";
+import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
+import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import { Hidden, Button, Paper } from "@material-ui/core";
 
 const styles = {
     termsBackground: {
@@ -84,7 +89,8 @@ class Terms extends Component {
         super(props);
 
         this.state = {
-            acceptedPolicies: []
+            acceptedPolicies: [],
+            expandedPolicies: {}
         };
     }
 
@@ -97,12 +103,28 @@ class Terms extends Component {
         const { policies, classes, error } = this.props;
         const { closeDialog } = this;
         const allAccepted = this.allPoliciesAccepted();
+        const { expandedPolicies } = this.state;
 
         return (
             <div className={ classes.termsContainer }>
                 <TopBarNoUser />
 
                 <div className={ classes.termsBackground }>
+                    <Grid container spacing={24}>
+                        <Grid item xs={12}>
+                            <Paper>
+                                <Typography>
+                                    Important, please read to ensure you understand how your information will be processed 
+                                </Typography>
+                            </Paper>
+                        </Grid>
+
+                        <Grid item xs={12}></Grid>
+                        <Button onClick={() => this.setState({ expandedPolicies: {} })}>
+                            Collapse All
+                        </Button>
+                    </Grid>
+
                     <Grid container spacing={24} className={ classes.contentContainer }>
                         <Grid container spacing={24} className={ classes.policiesContainer }>
                             {
@@ -123,13 +145,36 @@ class Terms extends Component {
                                 policies.map((p, key) => {
                                     return (
                                         <Grid item xs={12} sm={12} md={6} lg={6} key={key} className={ classes.policyAndAcceptContainer }>
-                                            <Card className={ classes.policyContainer }>
-                                                <div className={ classes.declarationContainer }>
-                                                    <Typography>
-                                                        <div dangerouslySetInnerHTML={{__html: p.narrative}}></div>
-                                                    </Typography>
-                                                </div>
-                                            </Card>
+                                            <Hidden mdUp>
+                                                <ExpansionPanel 
+                                                    style={{ borderRadius: 4, marginBottom: 16 }} 
+                                                    expanded={expandedPolicies[key] === true} 
+                                                    onChange={() => this.handleExpand(key)}>
+                                                    <ExpansionPanelSummary
+                                                        expandIcon={<ExpandMoreIcon />}
+                                                        aria-controls={`panel-${key}-content`}
+                                                        id={`panel-${key}-header`}
+                                                    >
+                                                        <Typography className={classes.heading}>{p.name}</Typography>
+                                                    </ExpansionPanelSummary>
+                                                    <ExpansionPanelDetails style={{ maxHeight: 500, overflowY: "auto" }}>
+                                                        <Typography>
+                                                            <div dangerouslySetInnerHTML={{__html: p.narrative}}></div>
+                                                        </Typography>
+                                                    </ExpansionPanelDetails>
+                                                </ExpansionPanel>
+                                            </Hidden>
+
+                                            <Hidden smDown>
+                                                <Card className={ classes.policyContainer }>
+                                                    <div className={ classes.declarationContainer }>
+                                                        <Typography>
+                                                            <div dangerouslySetInnerHTML={{__html: p.narrative}}></div>
+                                                        </Typography>
+                                                    </div>
+                                                </Card>
+                                            </Hidden>
+
                                             <Card className={ classes.policyAcceptContainer }>
                                                 <Checkbox
                                                     checked={this.policyAccepted(p)}
@@ -159,6 +204,14 @@ class Terms extends Component {
                 </div>
             </div>
         )
+    }
+
+    handleExpand = (key) => {
+        let { expandedPolicies } = this.state;
+
+        expandedPolicies[key] = !!!expandedPolicies[key];
+
+        this.setState({ expandedPolicies });
     }
 
     accept = () => {
